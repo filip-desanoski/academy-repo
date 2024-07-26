a7ceb3abd1f8b0af05bbcd4bb7ba34112cfcaf22
filip-desanoski <desanoskifilip@gmail.com>
2024-07-26 17:22:44 +0200

    Delete scheduler directory

--- scheduler/src/main/java/mk/iwec/scheduler/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.iwec.scheduler.repository;
-
-import mk.iwec.scheduler.model.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
-}
--- scheduler/src/main/java/mk/iwec/scheduler/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package mk.iwec.scheduler.repository;
-
-import mk.iwec.scheduler.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-}
